42b2728ba95854a716a22353777a51df809a9e4b
Bhavani Seleri <Bhavani.Seleri@teknopoint.local>
2026-09-25 12:47:41 +0530

    added target

--- blocks/search/search.js
+++ blocks/search/search.js
@@ -289,7 +289,7 @@ function searchBox(block, config) {
 
 export default async function decorate(block) {
   const placeholders = await fetchPlaceholders();
-  const source = block.querySelector('a[href]') ? block.querySelector('a[href]').href : '/query-index.json';
+  const source = block.querySelector('a[href]') ? block.querySelector('a[href]').href : '/en/query-index.json';
   block.innerHTML = '';
   // added by bs 
    block.classList.add('search-icon');
